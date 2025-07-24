877735d838c98fa3da4be8c163954f7bf7148236
Irma <ii@pryaniky.ru>
2025-07-24 18:50:28 +0300

    TweetForm

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,12 +1,18 @@
 import { ContentBox } from "@/components/ContentBox";
 import { Tweet } from "@/components/Tweet";
+import { TweetForm } from "@/components/TweetForm";
 import { TWEETS } from "@/shared/data/tweets.data";
 
 export default function Home() {
   return (
     <ContentBox
       title={`Home`}
-      children={TWEETS.map(tweet => <Tweet key={`key_id-${tweet.id}`} tweet={tweet} />)}
+      children={<>
+        <TweetForm />
+        {
+          TWEETS.map(tweet => <Tweet key={`key_id-${tweet.id}`} tweet={tweet} />)
+        }
+      </>}
     />
   );
 }
