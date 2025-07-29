febfbfd531f79d4680a86c965f826d027756a77c
Irma <ii@pryaniky.ru>
2025-07-29 17:59:36 +0300

    SSR@SSG shop

--- src/app/(public)/shop/ssr/page.tsx
+++ src/app/(public)/shop/ssr/page.tsx
@@ -1,3 +1,20 @@
-export default function ShopPage() {
-    return <div>SSR</div>
+import { ProductCard, type Product } from "@/components/ProductCard"
+import { API_URL } from "@/constant"
+
+export default async function shopSSRPage() {
+    const respons = await fetch(API_URL, {cache:"no-store"})
+    const products = (await respons.json()).products.map((product: any) => ({
+        id: product.id,
+        price: product.price,
+        image: product.thumbnail,
+        title: product.title
+    })) as Product[]
+    
+    return <div className="grid grid-cols-2 gap-4" >
+        {products.map(product => (
+            <ProductCard
+                key={product.id}
+                {...product} />
+        ))}
+    </div>
 }
