9ef4910514cd3008c3b8fde3c65ad1507847d499
Irma <ii@pryaniky.ru>
2025-07-29 17:49:17 +0300

    SSG page

--- src/app/(public)/shop/ssg/page.tsx
+++ src/app/(public)/shop/ssg/page.tsx
@@ -1,3 +1,20 @@
-export default function SSGPage() {
-    return <div>SSGGGGG</div>
+import { ProductCard, type Product } from "@/components/ProductCard"
+import { API_URL } from "@/constant"
+
+export default async function shopSSGPage() {
+    const respons = await fetch(API_URL)
+    const products = (await respons.json()).products.map((product: any) => ({
+        id: product.id,
+        price: product.price,
+        image: product.thumbnail,
+        title: product.title
+    })) as Product[]
+    
+    return <div className="grid grid-cols-4 gap-4">
+        {products.map(product => (
+            <ProductCard
+                key={product.id}
+                {...product} />
+        ))}
+    </div>
 }
