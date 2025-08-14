5541b2824ce8e19c9924b8e0ccba05a601c857bb
Irma <ii@pryaniky.ru>
2025-08-14 19:54:36 +0300

    add to imges

--- src/myComponents/ProductCard.tsx
+++ src/myComponents/ProductCard.tsx
@@ -9,7 +9,7 @@ export interface Product {
 
 export function ProductCard({ title, image, price }: Product) {
     return (
-        <div className="w-64  max-sm:w-full flex flex-col items-center border border-white/10 rounded-xl p-4 hover:shadow transition">
+        <div className="w-64  max-sm:w-full flex flex-col items-center border dark:border-white/10 rounded-xl p-4 hover:shadow transition">
             
             <Image
                 width={100}
@@ -17,7 +17,7 @@ export function ProductCard({ title, image, price }: Product) {
                 src={image}
                 alt={title}
                 className="object-contain mb-3 rounded" />
-            <h2 className="text-sm text-white dark:text-white font-medium line-clamp-2 mb-1">
+            <h2 className="text-sm  dark:text-white font-medium line-clamp-2 mb-1">
                 {title}
             </h2>
             <p className="text-sm text-gray-500 dark:text-gray-400">
